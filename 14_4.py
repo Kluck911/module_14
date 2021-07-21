acbafa8a8e13f08bfc18101205f98b7ba63c6b2c
kluck911 <kluck1812@gmail.com>
2021-07-21 15:45:03 +0300

    14.4.1 дополнено

--- 14_4.py
+++ 14_4.py
@@ -4,26 +4,28 @@ import time
 def decorator_time(fn):
    def wrapper():
        print(f"Запустилась функция {fn}")
-       dt_total = 0
-       for i in range(count) :
+       dt_mean = 0
+       for i in range(count):
            t0 = time.time()
            result = fn()
            dt = time.time() - t0
-           print(f"Функция выполнилась {i + 1} раз. Время: {dt:.10f}")
-           dt_total += dt_total + dt
-       print(f"Функция выполнилась {count} раз. Среднее время: {dt_total / count:.10f}")
+           dt_mean += dt
+       print(f"Функция выполнилась {count} раз. Среднее время: {dt_mean / count:.10f}")
        return dt  # задекорированная функция будет возвращать время работы
    return wrapper
 
 
 def pow_2():
-   return 10000000000000000000000000 ** 100000
+   return 10000000000000000000000000 ** 10000
 
 
 def in_build_pow():
-   return pow(10000000000000000000000000, 100000)
+   return pow(10000000000000000000000000, 10000)
 
 count = int(input(f'Введите кол-во выполнений = '))
+if count < 1:
+    print('Количество выполенний должно быть более 0')
+    raise SystemExit
 pow_2 = decorator_time(pow_2)
 in_build_pow = decorator_time(in_build_pow)
 pow_2()
