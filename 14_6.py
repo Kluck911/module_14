0d80621c62f7fc8b2dbb3d52b73bf3d112397121
kluck911 <kluck1812@gmail.com>
2021-07-23 13:47:16 +0300

    14_6_2 done

--- 14_6.py
+++ 14_6.py
@@ -1,9 +1,25 @@
-L = ['THIS', 'IS', 'LOWER', 'STRING']
+def even(x):
+    return x % 2 == 0
 
-print(list(map(str.lower, L)))
+
+some_list = [i - 10 for i in range(20)]
 
 
+def pow2(x): return x ** 2
+def positive(x): return x > 0
+
+
+print(list(map(pow2, filter(positive, some_list))))
+print([i**2 for i in some_list if i > 0])
 '''
+--------------------------------------------
+def pow2(x): return x ** 2
+def positive(x): return x > 0
+
+
+print(some_list)
+print(list(map(pow2, filter(positive, some_list))))
+print([i**2 for i in some_list if i > 0])
 ---------------------------------------------
 def even(x):
     return x % 2 == 0
@@ -12,4 +28,8 @@ def even(x):
 result = filter(even, [-2, -1, 0, 1, -3, 2, -3])
 print(list(result))
 ----------------------------------------------
+L = ['THIS', 'IS', 'LOWER', 'STRING']
+
+print(list(map(str.lower, L)))
+-----------------------------------------------
 '''
