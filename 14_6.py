d2e07f1f47815cb1041228838b5dfce06ff12752
kluck911 <kluck1812@gmail.com>
2021-07-23 13:12:29 +0300

    14_6_2 done

--- 14_6.py
+++ 14_6.py
@@ -1,3 +1,15 @@
 L = ['THIS', 'IS', 'LOWER', 'STRING']
 
 print(list(map(str.lower, L)))
+
+
+'''
+---------------------------------------------
+def even(x):
+    return x % 2 == 0
+
+
+result = filter(even, [-2, -1, 0, 1, -3, 2, -3])
+print(list(result))
+----------------------------------------------
+'''
--- temp.py
+++ temp.py
@@ -1,37 +1,6 @@
-USERS = ['admin', 'guest', 'director', 'root', 'superstar']
-
-yesno = input("""Введите Y, если хотите авторизоваться или N, 
-             если хотите продолжить работу как анонимный пользователь: """)
-
-auth = yesno == "Y"
-
-if auth:
-    username = input("Введите ваш username:")
+def even(x):
+    return x % 2 == 0
 
 
-def is_auth(func):
-    def wrapper():
-        if auth:
-            print("Пользователь авторизован")
-            func()
-        else:
-            print("Пользователь неавторизован. Функция выполнена не будет")
-    return wrapper
-
-
-def has_access(func):
-    def wrapper():
-        if username in USERS:
-            print("Авторизован как", username)
-            func()
-        else:
-            print("Доступ пользователю", username, "запрещен")
-    return wrapper
-
-
-@is_auth
-@has_access
-def from_db():
-    print("some data from database")
-
-from_db()
+result = filter(even, [-2, -1, 0, 1, -3, 2, -3])
+print(list(result))
